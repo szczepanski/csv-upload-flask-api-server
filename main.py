e782a99ba687adfb4830535d45473a26fd785e54
Piotr <ask.piotr@yahoo.co.uk>
2020-02-09 16:22:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,6 +65,29 @@ def s3BucketCreate(bucket_name, region):
         logging.error(e)
         return False
     return True
+		  
+
+def s3BucketFileUpload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True		  
 	  
 	  
 
@@ -111,6 +134,7 @@ def allowed_file(filename):
 
 s3BucketCreate('the-bla-buckets', 'eu-west-3')
 s3BucketList()
+s3BucketFileUpload_file(csv_file_path, 'the-bla-buckets', 'cities.json')
 
 
 
