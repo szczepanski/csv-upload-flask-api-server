0866d24deb1789760afc5e8aeadca28fb447d203
Piotr Szczepanski <ask.piotr@yahoo.co.uk>
2020-02-10 06:39:14 +0100

    update

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
 """
-
 csv file upload - flask api server
 converts csv file into jsnon and uploads to AWS S3 bucket
- 
 """
 
 __author__ = "Piotr Szczepanski"
 __version__ = "0.1.0"
 __license__ = "MIT"
-
 
 
 import os
@@ -41,15 +38,6 @@ def s3BucketList():
 		  
 		  
 def s3BucketCreate(bucket_name, region):
-    """Create an S3 bucket in a specified region
-
-    If a region is not specified, the bucket is created in the S3 default
-    region (us-east-1).
-
-    :param bucket_name: Bucket to create
-    :param region: String region to create bucket in, e.g., 'us-west-2'
-    :return: True if bucket created, else False
-    """
 
     # Create bucket
     try:
@@ -68,13 +56,6 @@ def s3BucketCreate(bucket_name, region):
 		  
 
 def s3BucketFileUpload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
 
     # If S3 object_name was not specified, use file_name
     if object_name is None:
@@ -88,43 +69,28 @@ def s3BucketFileUpload_file(file_name, bucket, object_name=None):
         logging.error(e)
         return False
     return True		  
-	  
-	  
-
-def csvToJson(csv_file_path, json_file_path):
-	data = {}
-
-	with open(csv_file_path) as csv_file:
-		csv_reader = csv.DictReader(csv_file)
-		for rows in csv_reader:
-			LatD = rows['LatD']
-			data[LatD] = rows
-	with open(json_file_path, 'w') as json_file:
-		json_file.write(json.dumps(data, indent=4))
-	
-		return json_file
 
 
 def converCsvToList(csv_file_path):
 	with open(csv_file_path) as f:
-		a = [{k: v for k, v in row.items()}
+		output_list = [{k: v for k, v in row.items()}
         for row in csv.DictReader(f, skipinitialspace=True)]
 	
-	for item in a:
+	# convert all numbers to integers
+	for item in output_list:
 		for key, value in item.items():
 			try:
 				item[key]=int(value)
 			except ValueError:
 				item[key] = value
-	print(a)
-	return a
+	return output_list
+
 
 def getFileMd5(csv_file_path):
 	hash_md5 = hashlib.md5()
 	with open(csv_file_path, "rb") as f:
 		for chunk in iter(lambda: f.read(4096), b""):
 			hash_md5.update(chunk)
-	print(hash_md5.hexdigest())
 	return hash_md5.hexdigest()
 
 
@@ -136,6 +102,7 @@ def allowed_file(filename):
 @app.route('/')
 def upload_form():
 	return render_template('upload.html')
+
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -155,10 +122,7 @@ def upload_file():
 
 			csv_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 			json_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'cities.json')
-			
-		
-			# s3BucketFileUpload_file(json_file_path, 'the-bla-buckets', 'cities.json')
-			
+						
 			
 			data = converCsvToList(csv_file_path)
 			md5 = getFileMd5(csv_file_path)
@@ -167,17 +131,16 @@ def upload_file():
 			with open(json_file_path, 'w') as f:
 				json.dump(output_json, f)
 			
-			
 			s3BucketCreate('the-bla-buckets', 'eu-west-3')
 			s3BucketList()
 			s3BucketFileUpload_file(json_file_path, 'the-bla-buckets', 'cities.json')
 
-			
 			return output_json
 
 		else:
 			flash('The allowed file type is csv. ')
 			return redirect(request.url)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
